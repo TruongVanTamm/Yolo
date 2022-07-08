969816544ea73d99692aa3130f03cd09563acc9b
Trương Văn Tâm <92572825+TruongVanTamm@users.noreply.github.com>
2022-07-08 11:58:48 +0700

    update8722

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -15,6 +15,7 @@ export const DataProvider = ({ children }) => {
     if (firstLogin) {
       const refreshToken = async () => {
         const res = await axios.get('/user/refresh_token');
+        console.log(res);
         setToken(res.data.access_token);
 
         setTimeout(() => {
